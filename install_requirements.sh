73d83d955297f37286a92e3dc1bb48fc1a9ad029
fadiabdulf <fadiabdulf@gmail.com>
2022-01-16 23:36:15 +0200

    Add docker

--- install_requirements.sh
+++ install_requirements.sh
@@ -8,5 +8,6 @@ pip install --update pip
 pip install -r requirements.txt
 
 python -c " import nltk; nltk.download('punkt'); nltk.download('stopwords')"
-python -m spacy download es_core_news_sm
-python -m spacy download xx_ent_wiki_sm
+python -m spacy download en_core_web_sm
+python -m spacy download es_core_news_md
+python -m spacy download xx_sent_ud_sm
